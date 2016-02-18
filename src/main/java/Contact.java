2dbc3d8eb6b44d70aa217de038d212ea40bcc63c
Abigail Rolling and Brad Pizzimenti <student@epicodus.com>
2016-02-18 09:07:36 -0800

    add full name method, add birth month

--- src/main/java/Contact.java
+++ src/main/java/Contact.java
@@ -3,9 +3,23 @@ import java.util.ArrayList;
 public class Contact {
   private String mFirstName;
   private String mLastName;
+  private String mBirthMonth;
 
   public Contact (String firstName, String lastName) {
     mFirstName = firstName;
     mLastName = lastName;
   }
-}
+
+  public String fullName() {
+    return mFirstName + " " + mLastName;
+  }
+
+  public void setBirthMonth(String month) {
+    mBirthMonth = month;
+  }
+
+  public String getBirthMonth() {
+    return mBirthMonth;
+  }
+
+} //end of Contact Class
--- src/test/java/ContactTest.java
+++ src/test/java/ContactTest.java
@@ -12,5 +12,17 @@ public class ContactTest {
     assertEquals(true, testContact instanceof Contact);
   }
 
+  @Test
+  public void Contact_returnsContactName_MarySmith() {
+    Contact testContact = new Contact("Mary","Smith");
+    assertEquals("Mary Smith", testContact.fullName());
+  }
+
+  @Test
+  public void getBirthMonth_addToContactObject_february() {
+    Contact testContact = new Contact("Mary","Smith");
+    testContact.setBirthMonth("February");
+    assertEquals("February", testContact.getBirthMonth());
+  }
 
 } //end of ContactTest
