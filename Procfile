e2d8b53dcc5e18fa7c3a38fa73956fbbb2c05b0a
Abigail Rolling and Brad Pizzimenti <arolling@gmail.com>
2016-02-18 16:36:23 -0800

    add heroku app deployment code

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: ./build/install/todo-list/bin/addressBook
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,17 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
+
+    ProcessBuilder process = new ProcessBuilder();
+     Integer port;
+     if (process.environment().get("PORT") != null) {
+         port = Integer.parseInt(process.environment().get("PORT"));
+     } else {
+         port = 4567;
+     }
+    setPort(port);
+
+
     String layout = "templates/layout.vtl";
     staticFileLocation("/public");
 
